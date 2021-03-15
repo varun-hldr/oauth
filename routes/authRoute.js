7c3d27e741343bacd36a84239a0fd54a41a8ad44
haldharvarun <haldharvarun@yahoo.com>
2021-03-15 17:10:02 +0530

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,28 +1,34 @@
-const passport = require('passport')
+const passport = require("passport");
 
-module.exports = (app)=>{
+module.exports = (app) => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  );
+  app.get("/auth/facebook", passport.authenticate("facebook"));
 
- app.get('/auth/google',passport.authenticate('google',{
-    scope:['profile','email']
-}))
-app.get('/auth/facebook',
-  passport.authenticate('facebook'));
+  app.get(
+    "/auth/google/redirect",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
+  app.get(
+    "/auth/facebook/redirect",
+    passport.authenticate("facebook", {
+      successRedirect: "/profile",
+    })
+  );
 
-app.get("/auth/google/callback",passport.authenticate('google'),(req,res)=>{
-    res.redirect('/profile')
-})
-app.get("/auth/facebook/callback",passport.authenticate('facebook',{
-    successRedirect:'/profile'
-}))
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 
-app.get('/api/current_user',(req,res)=>{
-    res.send(req.user)
-})
-
-app.get('/api/logout',(req,res)=>{
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.redirect('/')
-})
-
-}
-
+    res.redirect("/");
+  });
+};
